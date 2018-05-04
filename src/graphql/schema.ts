70d11b4472cf27f8224a75f0038605cf9b8eb7a7
Leonardo Cardoso <leocardosoTI@gmail.com>
2018-05-04 16:23:02 -0300

    implementando schema inicial

---
+++ src/graphql/schema.ts
@@ -0,0 +1,34 @@
+import { makeExecutableSchema } from 'graphql-tools';
+
+const users: any[] = [
+    {
+        id: 1,
+        name: 'Jon',
+        email: "email"
+    },
+    {
+        id: 1,
+        name: 'Jon',
+        email: "email"
+    }
+]
+
+const typeDefs = `
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+    }
+
+    type Query {
+        allUsers: [User!]!
+    }
+`
+
+const resolvers = {
+    Query: {
+        allUsers: () => []
+    }
+}
+
+export default makeExecutableSchema({ typeDefs, resolvers });
