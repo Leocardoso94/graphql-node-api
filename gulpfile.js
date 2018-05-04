0f17ec394cbbd0a3f0824990d1f7d0d9f1dfc8e3
Leonardo Cardoso <leocardosoTI@gmail.com>
2018-05-04 15:54:19 -0300

    implementando tarefas gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,24 @@ const ts = require('gulp-typescript');
 
 const tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('scripts', () => {
+const distFolder = 'dist';
+
+gulp.task('scripts', ['static'], () => {
     const tsResult = tsProject.src()
         .pipe(tsProject());
 
     return tsResult.js
-    .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest(distFolder))
 });
+
+gulp.task('static', ['clean'], () => gulp.src(['src/**/.json'])
+    .pipe(gulp.dest(distFolder)));
+
+gulp.task('clean', () => gulp.src(distFolder).pipe(clean()));
+
+gulp.task('build', ['scripts']);
+
+gulp.task('watch', ['build'], () => gulp
+    .watch(['src/**/*.ts', 'src/**/*.json'], ['build']));
+
+gulp.task('default', ['watch']);
