2f2f51d849ad7cb2bafc951e532ad6f0bd4cd7e0
Leonardo Cardoso <leocardosoTI@gmail.com>
2018-05-04 16:13:13 -0300

    congurando express

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-console.log('hi');
+import * as express from 'express';
+
+class App {
+    public express: express.Application
+
+    constructor() {
+        this.express = express();
+        this.middleware();
+    }
+
+    private middleware() {
+        this.express.use('/hello', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+            res.send({
+                hello: 'hello'
+            });
+        });
+    }
+}
+
+export default  new App().express;
