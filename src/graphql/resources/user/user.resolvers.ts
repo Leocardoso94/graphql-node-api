3cafec08d7fc25f4dc30ffd2816889ac42870ea5
Leonardo Cardoso <leocardosoti@gmail.com>
2018-05-16 22:54:17 -0300

    lista de posts no user schema e resolvers

--- src/graphql/resources/user/user.resolvers.ts
+++ src/graphql/resources/user/user.resolvers.ts
@@ -2,14 +2,25 @@ import { GraphQLResolveInfo } from 'graphql';
 import { DbConnectionInterface } from '../../../Interfaces/DbConnectionInterface';
 import { UserInstance } from '../../../models/UserModel';
 import { Transaction } from 'sequelize';
-import { EFBIG } from 'constants';
-
 
 const checkUser = (user, id) => {
   if (!user) throw new Error(`User with id ${id} not found`);
 };
 
 export const userResolvers = {
+  Users: {
+    posts: (
+      user: UserInstance, 
+      { first = 10, offset = 0 },
+      { db }: { db: DbConnectionInterface },
+      info: GraphQLResolveInfo, 
+    ) =>
+      db.Post.findAll({
+        offset,
+        where: { author: user.get('id') },
+        limit: first,
+      }),
+  },
   Query: {
     users: (
       parent,
@@ -17,23 +28,21 @@ export const userResolvers = {
       { db }: { db: DbConnectionInterface },
       info: GraphQLResolveInfo,
     ) => {
-      return db.User
-        .findAll({
-          offset,
-          limit: first,
-        });
+      return db.User.findAll({
+        offset,
+        limit: first,
+      });
     },
     user: (
       parent,
       { id },
       { db }: { db: DbConnectionInterface },
-      info: GraphQLResolveInfo) => {
-      return db.User
-        .findById(id)
-        .then((user: UserInstance) => {
-          checkUser(user, id);
-          return user;
-        });
+      info: GraphQLResolveInfo,
+    ) => {
+      return db.User.findById(id).then((user: UserInstance) => {
+        checkUser(user, id);
+        return user;
+      });
     },
   },
   Mutation: {
@@ -41,54 +50,51 @@ export const userResolvers = {
       parent,
       { input },
       { db }: { db: DbConnectionInterface },
-      info: GraphQLResolveInfo) => {
+      info: GraphQLResolveInfo,
+    ) => {
       return db.sequelize.transaction((transaction: Transaction) =>
-        db.User.create(input, { transaction }));
+        db.User.create(input, { transaction }),
+      );
     },
     updateUser: (
       parent,
       { id, input },
       { db }: { db: DbConnectionInterface },
-      info: GraphQLResolveInfo) => {
+      info: GraphQLResolveInfo,
+    ) => {
       return db.sequelize.transaction((transaction: Transaction) =>
-        db.User
-          .findById(Number(id))
-          .then((user: UserInstance) => {
-            checkUser(user, id);
-            return user.update(input, { transaction });
-          }),
+        db.User.findById(Number(id)).then((user: UserInstance) => {
+          checkUser(user, id);
+          return user.update(input, { transaction });
+        }),
       );
     },
     updateUserPassword: (
       parent,
       { id, input },
       { db }: { db: DbConnectionInterface },
-      info: GraphQLResolveInfo) => {
+      info: GraphQLResolveInfo,
+    ) => {
       return db.sequelize.transaction((transaction: Transaction) =>
-        db.User
-          .findById(Number(id))
-          .then((user: UserInstance) => {
-            checkUser(user, id);
-            return user
-              .update(input, { transaction })
-              .then((user: UserInstance) => !!user);
-          }),
+        db.User.findById(Number(id)).then((user: UserInstance) => {
+          checkUser(user, id);
+          return user
+            .update(input, { transaction })
+            .then((user: UserInstance) => !!user);
+        }),
       );
     },
     deleteUser: (
       parent,
       { id },
       { db }: { db: DbConnectionInterface },
-      info: GraphQLResolveInfo) => {
+      info: GraphQLResolveInfo,
+    ) => {
       return db.sequelize.transaction((transaction: Transaction) =>
-        db.User
-          .findById(Number(id))
-          .then((user: UserInstance) => {
-            checkUser(user, id);
-            return user
-              .destroy({ transaction })
-              .then(user => !!user);
-          }),
+        db.User.findById(Number(id)).then((user: UserInstance) => {
+          checkUser(user, id);
+          return user.destroy({ transaction }).then(user => !!user);
+        }),
       );
     },
   },
